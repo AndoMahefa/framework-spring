c5b6d81a9b7c3fbd4d611fb2cbbe721b3699b81e
AndoMahefa <razafimahatratramahefa7@gmail.com>
2023-03-30 22:37:45 +0300

    sprint4

---
+++ Sprint4/WEB-INF/src/Dept.java
@@ -0,0 +1,14 @@
+package models;
+
+import etu1896.annotation.Anno_Url;
+
+public class Dept {
+    @Anno_Url(url = "all-dept")
+    public void find_all_dept() {
+
+    }
+
+    public void insert() {
+
+    }
+}
---
+++ Sprint4/WEB-INF/src/Emp.java
@@ -0,0 +1,20 @@
+package models;
+
+import etu1896.annotation.Anno_Url;
+
+public class Emp {
+    
+    @Anno_Url(url = "all-emp")
+    public void find_all_emp(){
+
+    }
+
+    @Anno_Url(url = "insert-emp")
+    public void insert_emp(){
+
+    }
+
+    public void test(){
+        
+    }
+}
